aa7e6df6c35f81ce738fd699e2d87afadeaa01cf
lightknight64bit <gs4459@srmist.edu.in>
2021-02-14 23:42:29 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pickle
+from flask import Flask, request, jsonify, render_template
+app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+symptoms=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
+'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
+'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
+'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
+'fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool',
+'irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs',
+'swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails',
+'swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips',
+'slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints',
+'movement_stiffness','spinning_movements','loss_of_balance','unsteadiness',
+'weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine',
+'continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)',
+'depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain',
+'abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum',
+'rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion',
+'receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen',
+'history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf',
+'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
+'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
+'yellow_crust_ooze']
+
+disease=['Fungal infection','Allergy','GERD','Chronic cholestasis','Drug Reaction',
+'Peptic ulcer diseae','AIDS','Diabetes','Gastroenteritis','Bronchial Asthma','Hypertension',
+' Migraine','Cervical spondylosis',
+'Paralysis (brain hemorrhage)','Jaundice','Malaria','Chicken pox','Dengue','Typhoid','hepatitis A',
+'Hepatitis B','Hepatitis C','Hepatitis D','Hepatitis E','Alcoholic hepatitis','Tuberculosis',
+'Common Cold','Pneumonia','Dimorphic hemmorhoids(piles)',
+'Heartattack','Varicoseveins','Hypothyroidism','Hyperthyroidism','Hypoglycemia','Osteoarthristis',
+'Arthritis','(vertigo) Paroymsal  Positional Vertigo','Acne','Urinary tract infection','Psoriasis',
+'Impetigo']
+
+def symptom_to_disease(symptom_list):
+    bin_list = np.zeros(len(symptoms)).astype(int)
+    for k in range(len(symptoms)):
+        for z in symptom_list:
+            if z == symptoms[k]:
+                bin_list[k] = 1
+    if np.sum(bin_list) == 0:
+        return "Please select something"
+    return disease[model.predict([bin_list])[0]]
+
+
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if request.method == "POST":
+        symptoms = [x for x in request.form.values()]
+        
+
+        return render_template("AI DOCTOR.html", data=symptom_to_disease(symptoms))
+    return render_template("AI DOCTOR.html")
+@app.route('/home')
+def home1():
+    return render_template("home.html")
+@app.route('/disease')
+def disease_page():
+    return render_template("Disease (m).html")
+
+
+app.run(port=3000)
